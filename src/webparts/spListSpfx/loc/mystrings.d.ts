7439d8cb6a1dee9ea6b6f1d447da7f9e505f9e7a
Vishal Patil <vapatil7796@outlook.com>
2020-10-10 19:51:14 +0530

    CRUD on SharePoint list using SPFx

---
+++ src/webparts/spListSpfx/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpListSpfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpListSpfxWebPartStrings' {
+  const strings: ISpListSpfxWebPartStrings;
+  export = strings;
+}
